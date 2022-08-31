05402ccee6880e251cbb5a5083547078c23c63d5
Marcio Filho <marciofilho71@gmail.com>
2022-08-31 16:30:29 -0300

    Tela Padrão

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+#define WIDTH_GL 640
+#define HEIGHT_GL 480
+
+#define WIDTH_PC glutGet(GLUT_SCREEN_WIDTH)
+#define HEIGHT_PC glutGet(GLUT_SCREEN_HEIGHT)
+
+int main(int argc, char **argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+
+    glutInitWindowSize(WIDTH_GL, HEIGHT_GL);
+    glutInitWindowPosition((WIDTH_PC - WIDTH_GL) / 2, (HEIGHT_PC - HEIGHT_GL) / 2);
+
+    //init();
+    
+    /*glutKeyboardFunc(keyboard);
+    glutSpecialFunc(special);
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);*/
+
+    glutMainLoop();
+
+    return 0;
+}
